541194a5a4e9cef670abc11052eb9b6ce1630d26
Yh Qiao <snailknightqyh@gmail.com>
2024-09-23 21:56:37 +0800

    240923

---
+++ testmuduo/muduo_server.cpp
@@ -0,0 +1,18 @@
+/*muduo库主要的两个类：
+    TcpServer
+    TcpClient
+    能把网络I/O的代码和业务代码区分开
+        把 用户的连接和断开 和 用户的可读写事件 暴露出来
+
+*/
+#include <muduo/net/TcpServer.h>
+#include <muduo/net/EventLoop.h>
+#include <iostream>
+
+using namespace std;
+
+//基于
+class ChatServer
+{
+
+};
