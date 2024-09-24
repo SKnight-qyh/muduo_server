622516dfa88d2ac4c66d59fb54c9813c1ac1ef22
Yh Qiao <snailknightqyh@gmail.com>
2024-09-24 16:14:23 +0800

    240924

--- testmuduo/muduo_server.cpp
+++ testmuduo/muduo_server.cpp
@@ -9,10 +9,11 @@
 #include <muduo/net/EventLoop.h>
 #include <functional>
 #include <iostream>
+#include <string>
 using namespace std;
 using namespace placeholders;
-using namespace nuduo::net::TcpServer;
-using namespace nuduo::net;
+using namespace muduo;
+using namespace muduo::net;
 
 //基于muduo网络库开发服务器程序
 //1. 组合TcpServer对象
@@ -23,7 +24,7 @@ using namespace nuduo::net;
 class ChatServer
 {
 public:
-    ChatServer(EventLoop* loop, //时间循环
+    ChatServer(EventLoop* loop, //事件循环
             const InetAddress& listenAddr,  //IP + port
             const string& nameArg)      //服务器名称
             : _server(_loop, listenAddr, nameArg)
@@ -38,24 +39,50 @@ public:
                 //设置线程数量
                 _server.setThreadNum(3);    //muduo库会自适应，将有1个IO线程，2个工作线程
             }
-    //开启事件循环
+    //启动服务器
     void start(){
         _server.start();
     }
 private:
     //专门处理用户的连接创建和断开 epoll lintenfd accept
-    void onConnection(const TcpConnectionPtr &conn) {
-        
+    void onConnection(const TcpConnectionPtr& conn) {
+        if(conn->connected())
+        {
+            cout << conn->peerAddress().toIpPort() << " -> "
+            << conn->localAddress().toIpPort()  
+            << "state : online" << endl;
+        }
+        else
+        {
+            cout << conn->peerAddress().toIpPort() << " -> "
+            << conn->localAddress().toIpPort()  
+            << "state : offline" << endl;
+            conn->shutdown();   //close(connfd)
+            //_loop->quit();  // fd epoll_ctl_del => epoll
+        }
         
     }
 
     //专门处理用户的读写事件
     void onMessage(const TcpConnectionPtr &conn,    //连接
-                            Buffer *buf,    //缓冲区
+                            Buffer *buffer,    //缓冲区
                             Timestamp time) //接收到数据的时间信息
-                            {
-
-                            }
+    {
+        string buf = buffer->retrieveAllAsString();
+        cout << "recv data:" << buf << time.toString() << endl;
+        conn->send(buf);
+    }
     muduo::net::TcpServer _server;  //#1
     muduo::net::EventLoop *_loop;   //#2 epoll
 };
+
+int main()
+{
+    EventLoop loop;
+    InetAddress addr("127.0.0.1", 6000);
+    ChatServer server(&loop, addr, "ChatServer");
+    //启动服务区，并开启事件循环
+    server.start(); //lintenfd epoll_ctl => epoll
+    loop.loop();    //epoll_wait以阻塞方式等待新用户连接，已连接用户的读写事件
+
+}
